e281e64a244817f969e100b0bc053cc151d86a27
Cristian Aton <cristianeduardo0414@hotmail.com>
2024-08-15 03:48:39 -0400

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -4,5 +4,42 @@ import './menu.js';
 import {home} from './home.js';
 import {menu} from './menu.js';
 
+(function tabSwitch() {
+    const content = document.getElementById("content");
+    const homeBtn = document.getElementById("homeBtn");
+    const menuBtn = document.getElementById("menuBtn");
+    let isHomeActive = true;
+    let isMenuActive = false;
 
-menu()
+    homeBtn.addEventListener("click", function() {
+    if(isHomeActive === true) {
+        return;
+    } else if(isHomeActive === false) {
+        isHomeActive = true;
+        isMenuActive = false;
+
+        while (content.firstChild) {
+            content.removeChild(content.firstChild);
+        }
+        
+        home();
+    }
+    })
+
+    menuBtn.addEventListener("click", function() {
+        if(isMenuActive === true) {
+            return;
+        } else if(isMenuActive === false) {
+            isMenuActive = true;
+            isHomeActive = false;
+
+            while (content.firstChild) {
+                content.removeChild(content.firstChild);
+            }
+
+            menu();         
+        }
+        })
+
+    home()
+})();
